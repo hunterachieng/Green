4c57309704163f63fac2ad000d2948a5ce036c1c
hunterachieng <achienghunter@gmail.com>
2021-06-02 20:08:09 +0300

    green

--- main.js
+++ main.js
@@ -1,54 +1,3 @@
-const dots = document.getElementById("dots");
-const more = document.getElementById("more");
-const button = document.getElementById("read-more1");
-//adding event to buttons
-//first button
-button.addEventListener("click",function (){
-    if(dots.style.display === "none"){
-        dots.style.display = "inline";
-        button.textContent = "Read more";
-        more.style.display = "none";
-    }
-    else{
-        dots.style.display = "none";
-        button.textContent = "Read less";
-        more.style.display = "inline";
-    }
-});
-//second button
-const dots2 = document.getElementById("dots2");
-const more2 = document.getElementById("more2");
-const button2 = document.getElementById("read-more2");
-button2.addEventListener("click",function (){
-    if(dots2.style.display === "none"){
-        dots2.style.display = "inline";
-        button2.textContent = "Read more";
-        more2.style.display = "none";
-    }
-    else{
-        dots2.style.display = "none";
-        button2.textContent = "Read less";
-        more2.style.display = "inline";
-    }
-});
-
-//third button
-const dots3= document.getElementById("dots3");
-const more3 = document.getElementById("more3");
-const button3 = document.getElementById("read-more3");
-button3.addEventListener("click",function (){
-    if(dots3.style.display === "none"){
-        dots3.style.display = "inline";
-        button3.textContent = "Read more";
-        more3.style.display = "none";
-    }
-    else{
-        dots3.style.display = "none";
-        button3.textContent = "Read less";
-        more3.style.display = "inline";
-    }
-});
-
 const navBar = document.getElementById("nav-bar");
 const btn = document.getElementById("btntoggle");
 btn.addEventListener("click", function(){
@@ -77,4 +26,53 @@ e.addEventListener("click" , function(){
 })
 
 })
+
+const slides = document.getElementsByClassName("pic-slides");
+const dots = document.getElementsByClassName("dots");
+var slideIndexes = 0;
+
+function slidesShow(){
+    let i;
+    for (i = 0; i< slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideIndexes ++;
+    if(slideIndexes > slides.length){
+        slideIndexes = 1;
+    }
+    for(i =0;i<dots.length; i++){
+        if(dots[i].classList.contains("active")){
+            dots[i].classList.remove("active");
+        }
+        else{
+            dots[slideIndexes-1].classList.add("active");
+        }
+    }
+    slides[slideIndexes-1].style.display = "block";
+    setTimeout(slidesShow, 2000);
+}
+slidesShow();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
